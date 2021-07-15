3f06b70d3bf3dcd7f63f781292706077ecb76bb3
Lauren Hutchinson <lbhutchinson022@gmail.com>
2021-07-15 13:47:08 -0700

    fakeshop tutorial finished

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
+import { setProducts } from "../redux/actions/productActions";
 import axios from "axios";
 
 const ProductListing = () => {
-  const products = useSelector(state => state);
+  const products = useSelector(state => state.allProducts.products);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch(err => {
+        console.log("Err: ", err);
+      });
+    dispatch(setProducts(response.data));
+  };
 
-  console.log(products);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Products :", products);
   return (
     <div className="ui grid container">
       <ProductComponent />
